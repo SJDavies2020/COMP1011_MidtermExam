c48790e794c82678dba48fcc176a203bba6d910e
SJDavies2020 <200411361@student.georgianc.on.ca>
2020-10-30 12:11:58 -0400

    Complete Model Class

--- src/AthleteModel.java
+++ src/AthleteModel.java
@@ -18,63 +18,104 @@ public class AthleteModel {
         setRun1(runRace1);
         setRun2(runRace2);
         }
-    /**
-     * Create Getters and Setters and do validation and Execption Handeling
-     */
 
+    /**
+     * 
+     * @return
+     */
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
-        this.id = id;
+        if (id == 0) {
+            throw new IllegalArgumentException("Id must be Grater than 0");
+        }
+        else{
+            this.id = id;
+        }
     }
-
     public int getYear() {
         return year;
     }
-
     public void setYearofBirth(int year) {
-        this.year = year;
+        if (year == 2004 || year == 2005)
+        {
+            this.year = year;
+        }
+        else{
+            throw new IllegalArgumentException("Birth Year must be 2004 or 2005");
+        }
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        if (firstName.matches("^([a-zA-Z]+?)([-\\s'][a-zA-Z]+)*?$")) {
+            this.firstName = firstName;
+        }
+        else{
+            throw new IllegalArgumentException("I\"Input must only be text. Starting with a uppercase Letter\"");
+        }
     }
-
     public String getLastName() {
+
         return lastName;
     }
-
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        if (lastName.matches("^([a-zA-Z]+?)([-\\s'][a-zA-Z]+)*?$")) {
+            this.lastName = lastName;
+        }
+        else{
+            throw new IllegalArgumentException("Input must only be text. Starting with a uppercase Letter");
+        }
     }
-
     public String getGender() {
         return gender;
     }
-
     public void setGender(String gender) {
-        this.gender = gender;
+        if (gender.matches("^male$|^female$")) {
+            this.gender = gender;
+        }
+        else{
+            throw new IllegalArgumentException("Gender must be Lowercase and male or female");
+        }
     }
-
     public double getRun1() {
         return run1;
     }
-
     public void setRun1(double run1) {
-        this.run1 = run1;
+        if (run1 <= 40) {
+          this.run1 = run1;
+        }
+        else{
+            throw new IllegalArgumentException("The Run duration must be Greater than or Equal to 40");
+        }
     }
-
     public double getRun2() {
         return run2;
     }
-
     public void setRun2(double run2) {
-        this.run2 = run2;
+        if (run2 <= 40) {
+            this.run2 = run2;
+        }
+        else{
+            throw new IllegalArgumentException("The Run duration must be Greater than or Equal to 40");
+        }
+
     }
+    public double getCombinedTime(double run1, double run2)
+    {
+        double totalTime = run1 + run2;
+        return totalTime;
+    }
+        public String CombinedTimeString(double TotalTime)
+    {
+        String result = String.valueOf(TotalTime);
+        return result;
+    };
+    
+    
+    
+    
+    
 }
